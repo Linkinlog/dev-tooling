186c42efa8dc80691c770fe3cdc7dc4e9f75e0c5
Linkinlog <dahlton@linkd.dev>
2022-12-27 12:29:20 -0500

    WIP: Got rough base line

---
+++ assible/go.mod
@@ -0,0 +1,3 @@
+module github.com/Linkinlog/assible
+
+go 1.19
---
+++ assible/pkg/commandProcessor/process.go
@@ -0,0 +1,50 @@
+// commandprocessor is the first logical gate our tool encounters,
+// it provides utilities to  determine if we should go through standard initialization, or
+// go through setup using the provided command flags.
+package commandprocessor
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Linkinlog/assible/pkg/osHandler"
+)
+
+// ProcessCommand will start everything,
+// handling the flags/args and processing.
+func ProcessCommand() error {
+	systemPtr := flag.String("system", "linux", "The target OS")
+	if os.Args[0] == "init" {
+		return initTool()
+	} else {
+		var system, err = osHandler.StringToSystem(*systemPtr)
+		if err != nil {
+			return err
+		}
+		return startPipeline(system)
+	}
+}
+
+// initTool will collect options from the user
+// and pass them along to the pipeline.
+func initTool() error {
+	var prompt string
+	fmt.Print("Input OS(Win, Linux, Mac): ")
+	fmt.Scanf("%s", &prompt)
+	// Make a new os struct with the prompt.
+	var system, err = osHandler.StringToSystem(prompt)
+	if err != nil {
+		return err
+	}
+	startPipeline(system)
+	return nil
+}
+
+// startPipeline will take a look at what arguments were passed in
+// and then begin setting up shop, starting with installing pacakages.
+func startPipeline(system osHandler.System) error {
+	// Pass along to installer service.
+	return errors.New("WIP")
+}
